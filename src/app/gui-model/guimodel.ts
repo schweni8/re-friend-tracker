32e8a168e33fa642bf8f93e33362132a27d02303
schweni8 <schweni8@students.zhaw.ch>
2021-12-09 18:25:01 +0000

    änderung

--- src/app/gui-model/guimodel.ts
+++ src/app/gui-model/guimodel.ts
@@ -6,7 +6,7 @@ export class GuiModel {
 
     private _guiModel = {
         "application": {
-            "title": "Requirements Engineering Friend Tracker",
+            "title": "REng Friend Tracker",
             "formList": [
                 {
                     "id": "FriendForm",
